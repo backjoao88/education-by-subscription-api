2284237a38fda5173a336ee82ea8dc78d17506f3
Back <joaoback47@gmail.com>
2024-06-23 09:46:57 -0300

    fix: response from storage provider interface send method

--- EducationBySubscription.Application/Providers/Storage/IStorageProvider.cs
+++ EducationBySubscription.Application/Providers/Storage/IStorageProvider.cs
@@ -2,6 +2,6 @@
 
 public interface IStorageProvider
 {
-    public Task Send(Guid idCourse, byte[] byteChunk);
+    public Task<SendStorageProviderResponse> Send(Guid idCourse, byte[] byteChunk);
     public Task<byte[]> Retrieve(Guid idCourse);
 }

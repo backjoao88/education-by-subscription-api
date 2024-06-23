e5abbdb3cf8855eece4357ee3fbf73849abbdd28
Back <joaoback47@gmail.com>
2024-06-23 09:49:02 -0300

    add: allowed courses property

--- EducationBySubscription.Core/Domain/Plans/Plan.cs
+++ EducationBySubscription.Core/Domain/Plans/Plan.cs
@@ -4,15 +4,17 @@ namespace EducationSubscription.Core.Domain.Plans;
 
 public class Plan : Entity
 {
-    public Plan(string title, string description, decimal basePrice)
+    public Plan(string title, string description, decimal basePrice, Guid[] allowedCourses)
     {
         Title = title;
         Description = description;
         BasePrice = basePrice;
+        AllowedCourses = allowedCourses;
     }
     public string Title { get; private set; }
     public string Description { get; private set; }
     public decimal BasePrice { get; private set; }
+    public Guid[] AllowedCourses { get; private set; }
 
     public void Update(string title, string description, decimal basePrice)
     {
